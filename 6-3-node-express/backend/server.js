165a325997d6142f57f07bf6bfcf2b2189b1c8cc
zainab14-48 <s202251360@kfupm.edu.sa>
2026-04-06 13:02:22 +0300

    server.js

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,18 +1,33 @@
 // TODO 1: Import and Create express app instance
+import express from "express";
+const app = express();
 
 // TODO 1: Define server port
-
+const PORT = 3000;
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
+import morgan from "morgan";
+app.use(morgan("dev"));
+import { getRandomQuote } from "./quotes.js";
+
 
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
 
 // TODO 6.2: Create "/api/quote" route
-
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 // TODO 7: Start server using app.listen
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
